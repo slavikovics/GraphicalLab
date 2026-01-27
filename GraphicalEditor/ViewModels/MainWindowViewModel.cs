c5fddae648dc4f8b415dda390ec26e1fdaa188b9
slavikovics <slavikovics@outlook.com>
2026-01-28 00:25:07 +0300

    Adding writable bitmap

--- GraphicalEditor/ViewModels/MainWindowViewModel.cs
+++ GraphicalEditor/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using Avalonia;
 using Avalonia.Controls.Notifications;
+using Avalonia.Media.Imaging;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using SukiUI.Toasts;
@@ -10,51 +12,40 @@ public partial class MainWindowViewModel : ViewModelBase
 {
     public ISukiToastManager ToastManager { get; } = new SukiToastManager();
 
-    [RelayCommand]
-    private void Error()
+    [ObservableProperty] private WriteableBitmap _bitmap = new(new PixelSize(300, 300), new Vector(300, 300));
+
+    private void ShowToast(string title, string content, NotificationType notificationType)
     {
         var builder = ToastManager.CreateToast();
-        builder.SetContent("An error occured");
-        builder.SetTitle("Error");
-        builder.SetType(NotificationType.Error);
+        builder.SetContent(content);
+        builder.SetTitle(title);
+        builder.SetType(notificationType);
         builder.SetCanDismissByClicking(true);
         builder.SetDismissAfter(TimeSpan.FromSeconds(2));
         builder.Queue();
+    }
+
+    [RelayCommand]
+    private void Error()
+    {
+        ShowToast("Error", "An error occured", NotificationType.Error);
     }
     
     [RelayCommand]
     private void Warning()
     {
-        var builder = ToastManager.CreateToast();
-        builder.SetContent("A warning occured");
-        builder.SetTitle("Warning");
-        builder.SetType(NotificationType.Warning);
-        builder.SetCanDismissByClicking(true);
-        builder.SetDismissAfter(TimeSpan.FromSeconds(2));
-        builder.Queue();
+        ShowToast("Warning", "A warning occured", NotificationType.Warning);
     }
     
     [RelayCommand]
     private void Info()
     {
-        var builder = ToastManager.CreateToast();
-        builder.SetContent("An info occured");
-        builder.SetTitle("Info");
-        builder.SetType(NotificationType.Information);
-        builder.SetCanDismissByClicking(true);
-        builder.SetDismissAfter(TimeSpan.FromSeconds(2));
-        builder.Queue();
+        ShowToast("Info", "An info occured", NotificationType.Information);
     }
     
     [RelayCommand]
     private void Success()
     {
-        var builder = ToastManager.CreateToast();
-        builder.SetContent("A success occured");
-        builder.SetTitle("Success");
-        builder.SetType(NotificationType.Success);
-        builder.SetCanDismissByClicking(true);
-        builder.SetDismissAfter(TimeSpan.FromSeconds(2));
-        builder.Queue();
+        ShowToast("Success", "A success occured", NotificationType.Success);
     }   
 }
